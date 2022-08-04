api7/grpc-client-nginx-module

commit 2e5e4183891b409c20b50130c05f469b7e0ff830

fix: only one coroutine can call at the same time (#1)
Signed-off-by: spacewander <spacewanderlzx@gmail.com>

src/ngx_http_grpc_client.c
@@ -37,6 +37,8 @@ typedef struct {
     int                           state;
 
     ngx_rbtree_node_t            *node;
+
+    unsigned                      waiting:1;
 } ngx_http_grpc_cli_ctx_t;
 
 
@@ -362,6 +364,7 @@ ngx_http_grpc_cli_resume(ngx_http_request_t *r)
     }
 
     ctx = lctx->cur_co_ctx->data;
+    ctx->waiting = 0;
     lctx->cur_co_ctx->data = ctx->prev_data;
     res = &ctx->res;
 
@@ -448,7 +451,6 @@ static void
 ngx_http_grpc_cli_thread_post_event_handler(ngx_event_t *ev)
 {
     ngx_http_grpc_cli_thread_ctx_t       *thctx = ev->data;
-    ngx_log_t                            *log = ev->log;
     ngx_queue_t                          *q;
     ngx_http_grpc_cli_ctx_t              *ctx;
     ngx_http_grpc_cli_posted_event_ctx_t *posted_event_ctx;
@@ -708,6 +710,11 @@ ngx_http_grpc_cli_call(unsigned char *err_buf, size_t *err_len,
 
     gccf = ngx_http_cycle_get_module_main_conf(ngx_cycle, ngx_http_grpc_client_module);
 
+    if (ctx->waiting) {
+        *err_len = ngx_snprintf(err_buf, *err_len, "busy waiting") - err_buf;
+        return NGX_ERROR;
+    }
+
     r = ctx->r;
     lctx = ngx_http_get_module_ctx(r, ngx_http_lua_module);
     if (lctx == NULL) {
@@ -726,6 +733,7 @@ ngx_http_grpc_cli_call(unsigned char *err_buf, size_t *err_len,
     ctx->err_buf = err_buf;
     ctx->err_len = err_len;
     ctx->state = NGX_HTTP_GRPC_CLIENT_STATE_OK;
+    ctx->waiting = 1;
 
 
     if (!gccf->task->event.active) {

t/bad.t
@@ -24,3 +24,40 @@ location /t {
 }
 --- response_body
 bad request
+
+
+
+=== TEST 2: two thread wait for one conn
+--- http_config
+server {
+    listen 2376 http2;
+
+    location / {
+        access_by_lua_block {
+            ngx.sleep(0.5)
+        }
+        grpc_pass         grpc://127.0.0.1:2379;
+    }
+}
+--- config
+location /t {
+    content_by_lua_block {
+        local gcli = require("resty.grpc")
+        assert(gcli.load("t/testdata/rpc.proto"))
+
+        local conn = assert(gcli.connect("127.0.0.1:2376"))
+        local function co()
+            local ok, err = conn:call("etcdserverpb.KV", "Put", {key = 'k', value = 'v'})
+            if not ok then
+                ngx.log(ngx.WARN, err)
+            end
+        end
+        local th1 = ngx.thread.spawn(co)
+        local th2 = ngx.thread.spawn(co)
+        ngx.thread.wait(th1, th2)
+    }
+}
+--- grep_error_log eval
+qr/failed to call: [^,]+/
+--- grep_error_log_out
+failed to call: busy waiting

commit 07361381ed480af7bbcb9f95a567714aa616e2bd

fix: the log variable is removed accidently
The bug is introduced in https://github.com/api7/grpc-client-nginx-module/commit/2e5e4183891b409c20b50130c05f469b7e0ff830
Signed-off-by: spacewander <spacewanderlzx@gmail.com>

src/ngx_http_grpc_client.c
@@ -464,8 +464,8 @@ ngx_http_grpc_cli_thread_post_event_handler(ngx_event_t *ev)
 
         res = &posted_event_ctx->res;
 
-        ngx_log_debug2(NGX_LOG_DEBUG_HTTP, log, 0, "resume finished task %uL, ctx:%p",
-                    res->task_id, posted_event_ctx);
+        ngx_log_debug2(NGX_LOG_DEBUG_HTTP, ev->log, 0, "resume finished task %uL, ctx:%p",
+                       res->task_id, posted_event_ctx);
 
         ctx = (ngx_http_grpc_cli_ctx_t *) res->task_id;
         ctx->res = *res;