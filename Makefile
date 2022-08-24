6861dab59ec6d021c2eb0974e4cf14d27d940f27
spacewander <spacewanderlzx@gmail.com>
2022-08-24 19:44:22 +0800

    fix: tweak installation script
    Signed-off-by: spacewander <spacewanderlzx@gmail.com>

--- Makefile
+++ Makefile
@@ -3,6 +3,6 @@ INSTALL ?= install
 
 .PHONY: install
 install:
-	if [ ! -f /usr/local/go/bin/go ]; then install-util.sh install_go; fi 
-	cd ./grpc-engine && /usr/local/go/bin/go build -o libgrpc_engine.so -buildmode=c-shared main.go
+	if [ ! -f /usr/local/go/bin/go ]; then ./install-util.sh install_go; fi
+	cd ./grpc-engine && PATH="$(PATH):/usr/local/go/bin" go build -o libgrpc_engine.so -buildmode=c-shared main.go
 	$(INSTALL) -m 664 ./grpc-engine/libgrpc_engine.so $(OPENRESTY_PREFIX)/
--- install-util.sh
+++ install-util.sh
@@ -1,11 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
+set -x
 
+arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+if [ "$arch" = "x86_64" ]; then
+    arch="amd64"
+fi
+if [ "$arch" = "aarch64" ]; then
+    arch="arm64"
+fi
 
 install_go() {
+    if grep "NAME=" /etc/os-release | grep  "Alpine"; then
+        # the official Go binary is linked with glibc, so we need to download
+        # it from the package manager
+        apk add --no-cache go
+        return
+    fi
+
     GO_VER=1.19
-    wget https://go.dev/dl/go${GO_VER}.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VER}.linux-amd64.tar.gz
+    wget --quiet https://go.dev/dl/go${GO_VER}.linux-${arch}.tar.gz > /dev/null
+    rm -rf /usr/local/go && tar -C /usr/local -xzf go${GO_VER}.linux-${arch}.tar.gz
     /usr/local/go/bin/go version
 }
 
